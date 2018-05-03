added projected gradient method, took adv ops out of graph_builder_wrapper (added a gradient wrt x op), modified fgm to compute adversaries in numpy instead of tensorflow, switched from fixed point method to gradient descent

spectral_adversarial_regularization/adversarial.py
@@ -6,92 +6,80 @@ import numpy as np
 import tensorflow as tf
 import dl_utils
 
-def fgm(x, preds, y=None, eps=0.3, order=np.inf, clip_min=None, clip_max=None):
+def fgm(x, graph, sess, eps=0.3, order=2, num_stability=1e-24):
     """
-    TensorFlow implementation of the Fast Gradient Method.
-    :param x: the input placeholder
-    :param preds: the model's output tensor
-    :param y: (optional) A placeholder for the model labels. Only provide
-              this parameter if you'd like to use true labels when crafting
-              adversarial samples. Otherwise, model predictions are used as
-              labels to avoid the "label leaking" effect (explained in this
-              paper: https://arxiv.org/abs/1611.01236). Default is None.
-              Labels should be one-hot-encoded.
-    :param eps: the epsilon (input variation parameter)
-    :param ord: (optional) Order of the norm (mimics Numpy).
-                Possible values: np.inf, 1 or 2.
-    :param clip_min: Minimum float value for adversarial example components
-    :param clip_max: Maximum float value for adversarial example components
-    :return: a tensor for the adversarial example
+    Fast Gradient Method for computing adv 
+    perturbation of x
     """
 
-    if y is None:
-        # Using model predictions as ground truth to avoid label leaking
-        y = tf.argmax(preds, 1)
-
-
-    # Compute loss (without taking the mean across samples)
-    loss_ = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,logits=preds)
-
-    # Define gradient of loss wrt input
-    grad, = tf.gradients(loss_, x)
-
+    g = sess.run(graph['x_grad'], feed_dict={graph['input_data']: x,
+                                             graph['delta']: np.zeros(np.shape(x))})
+    
     if order == np.inf:
-        # Take sign of gradient
-        signed_grad = tf.sign(grad)
+        adv_x = x + eps*np.sign(g)
+        
     elif order == 1:
-        reduc_ind = list(xrange(1, len(x.get_shape())))
-        signed_grad = grad / tf.reduce_sum(tf.abs(grad),
-                                           reduction_indices=reduc_ind,
-                                           keep_dims=True)
+        reduc_ind = tuple(xrange(1, len(x.shape)))
+        adv_x = x + eps*g/(np.sum(np.abs(g), axis=reduc_ind, keepdims=True)+num_stability)
+        
     elif order == 2:
-        reduc_ind = list(xrange(1, len(x.get_shape())))
-        signed_grad = grad / tf.sqrt(tf.reduce_sum(tf.square(grad),
-                                                   reduction_indices=reduc_ind,
-                                                   keep_dims=True))
+        reduc_ind = tuple(xrange(1, len(x.shape)))
+        adv_x = x + eps*g/np.sqrt(np.sum(np.square(g), axis=reduc_ind, keepdims=True)+num_stability)
+
     else:
         raise NotImplementedError("Only L-inf, L1 and L2 norms are "
                                   "currently implemented.")
 
-    # Multiply by constant epsilon
-    scaled_signed_grad = eps * signed_grad
-
-    # Add perturbation to original example to obtain adversarial example
-    adv_x = tf.stop_gradient(x + scaled_signed_grad)
-
-    # If clipping is needed, reset all values outside of [clip_min, clip_max]
-    if (clip_min is not None) and (clip_max is not None):
-        adv_x = tf.clip_by_value(adv_x, clip_min, clip_max)
-
     return adv_x
 
 
-def gen_adv_examples_in_sess(X, graph, sess, batch_size=100):
+def pgm(x, graph, sess, eps=0.3, k=15, a=0.01, order=2, num_stability=1e-24):
+    """
+    Projected Gradient Method
+    (referencing: https://github.com/MadryLab/mnist_challenge/blob/master/pgd_attack.py)
+    """
+    
+    adv_x = np.copy(x)
+    for i in range(k):
+        
+        g = sess.run(graph['x_grad'], feed_dict={graph['input_data']: adv_x,
+                                                 graph['delta']: np.zeros(np.shape(x))})
+        
+        if order == np.inf:
+            adv_x += a*np.sign(g)
+            adv_x = np.clip(adv_x, x-eps, x+eps) 
+            
+        elif order == 1:
+            reduc_ind = tuple(xrange(1, len(x.shape)))
+            adv_x += a*g/(np.sum(np.abs(g), axis=reduc_ind, keepdims=True)+num_stability)
+            u = adv_x-x
+            adv_x = x + eps*u/(np.sum(np.abs(u), axis=reduc_ind, keepdims=True)+num_stability)
+            
+        elif order == 2:
+            reduc_ind = tuple(xrange(1, len(x.shape)))
+            adv_x += a*g/np.sqrt(np.sum(np.square(g), axis=reduc_ind, keepdims=True)+num_stability)
+            u = adv_x-x
+            adv_x = x + eps*u/np.sqrt(np.sum(np.square(u), axis=reduc_ind, keepdims=True)+num_stability)
+            
+        else:
+            raise NotImplementedError("Only L-inf, L1 and L2 norms are "
+                                      "currently implemented.")
+
+    return adv_x
+    
+    
+def gen_adv_examples_in_sess(X, graph, sess, batch_size=100, method=fgm, **kwargs):
     """Use trained model to generate adversarial examples from X within a session"""
     
     adv_x = np.zeros(np.shape(X))
-    for i in range(0,len(X),batch_size):
-        adv_x_ = sess.run(graph['adv_x'], feed_dict = {graph['input_data']:X[i:i+batch_size], 
-                                                       graph['delta']:np.zeros(np.shape(X[i:i+batch_size]))})
-        adv_x[i:i+batch_size] = adv_x_
+    for i in range(0, len(X), batch_size):
+        adv_x[i:i+batch_size] = method(X[i:i+batch_size], graph, sess, **kwargs)
     return adv_x
-    
-
-def gen_adv_examples(X, graph, load_dir, sess=None, batch_size=100, load_epoch=None):
-    """Use trained model to generate adversarial examples from X"""
-
-    # Load from checkpoint corresponding to latest epoch if none given
-    if load_epoch == None:
-        load_epoch = max([int(f.split('epoch')[1].split('.')[0]) 
-                          for f in os.listdir(os.path.join(load_dir, 'checkpoints')) if 'epoch' in f])
-
-    with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
-        graph['saver'].restore(sess, os.path.join(load_dir, 'checkpoints', 'epoch%s'%(load_epoch)))
-        return adv_x(X, graph, sess)
 
 
-def test_net_against_adv_examples(X, Y, load_dir, arch, d=None, beta=1.,
-                                  eps=0.3, order=np.inf, gpu_id=0, verbose=True):
+def test_net_against_adv_examples(X, Y, load_dir, arch, d=None, beta=1., 
+                                  verbose=True, gpu_id=0, gpu_prop=0.2,
+                                  method=fgm, **kwargs):
     """For a trained network, generate and get accuracy for adversarially-perturbed samples"""
     
     num_classes = len(np.unique(Y))
@@ -100,10 +88,10 @@ def test_net_against_adv_examples(X, Y, load_dir, arch, d=None, beta=1.,
     # Use previously fitted network which had achieved 100% training accuracy
     tf.reset_default_graph()
     with tf.device("/gpu:%s"%(gpu_id)):
-        graph = dl_utils.graph_builder_wrapper(num_classes, load_dir, beta=beta, eps=eps, order=order,
-                                               arch=arch, update_collection='_')
+        graph = dl_utils.graph_builder_wrapper(num_classes, load_dir, beta=beta, arch=arch, update_collection='_')
 
-        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                              gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=gpu_prop))) as sess:
             if d is None:
                 load_epoch = dl_utils.latest_epoch(load_dir)
                 graph['saver'].restore(sess, os.path.join(load_dir, 'checkpoints', 'epoch%s'%(load_epoch)))
@@ -113,7 +101,7 @@ def test_net_against_adv_examples(X, Y, load_dir, arch, d=None, beta=1.,
                     sess.run(v.assign(d[v.name]))
             
             # Generate adversarial samples and predict
-            X_adv = gen_adv_examples_in_sess(X, graph, sess)
+            X_adv = gen_adv_examples_in_sess(X, graph, sess, method=method, **kwargs)
             Yhat_adv = dl_utils.predict_labels_in_sess(X_adv, graph, sess)
 
     accs_adv = np.sum(Yhat_adv == Y)/float(len(Y))

spectral_adversarial_regularization/dl_utils.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-from adversarial import fgm
+import adversarial as ad
 from sklearn.utils import shuffle
 
 import models.alexnet_small as model
@@ -29,12 +29,12 @@ def acc(g, Y):
 
 def graph_builder_wrapper(num_classes, save_dir,
                           wd=0,
-                          eps=0.3,
-                          order=np.inf,
-                          max_save=200,
+                          lambda_adv=2,
                           arch=model.alexnet,
                           update_collection=None,
-                          beta=1.):
+                          beta=1.,
+                          save_histograms=False,
+                          max_save=200):
     """Wrapper for building graph and accessing all relevant ops/placeholders"""
     
     input_data = tf.placeholder(tf.float32, shape=[None, 28, 28, 3], name='in_data')
@@ -50,27 +50,32 @@ def graph_builder_wrapper(num_classes, save_dir,
     opt_step = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(total_loss)
     tf.summary.scalar('loss', total_loss)
     
-    # Adv robustness using Duchi et al.'s ICLR 2018 result
-    adv_loss = -tf.nn.sparse_softmax_cross_entropy_with_logits(labels=input_labels, logits=fc_out)
-    delta_grad = tf.gradients(adv_loss, delta)[0]
+    # Adv robustness using Sinha et al.'s ICLR 2018 result
+    wrm_loss = -tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=input_labels,
+                                                               logits=fc_out, name='wrm_loss')) + lambda_adv*tf.reduce_mean(tf.reduce_sum(delta**2, reduction_indices=[1, 2, 3]))
+    delta_grad = tf.gradients(wrm_loss, delta)[0]
+    
+    # Gradient with respect to input useful for finding adversarial examples
+    adv_loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=tf.argmax(fc_out, 1),
+                                                              logits=fc_out, name='adv_loss')
+    x_grad = tf.gradients(adv_loss, input_data)[0]
 
-    # Accuracy
+    # Compute accuracy
     total_acc = acc(fc_out, input_labels)
     tf.summary.scalar('accuracy', total_acc)
     
-    # Add histograms for trainable variables.
-#     for var in tf.trainable_variables():
-#         tf.summary.histogram(var.op.name, var)
+    # Add histograms for trainable variables (really slows down training though)
+    if save_histograms:
+        for var in tf.trainable_variables():
+            tf.summary.histogram(var.op.name, var)
 
     # Merge all the summaries and write them out to save_dir
     merged = tf.summary.merge_all()
     train_writer = tf.summary.FileWriter(os.path.join(save_dir, 'train'))
     graph_writer = tf.summary.FileWriter(os.path.join(save_dir, 'graph'), graph=tf.get_default_graph())
     valid_writer = tf.summary.FileWriter(os.path.join(save_dir, 'validation'))
-
-    # Fast gradient method for finding adversarial examples
-    adv_x = fgm(input_data, fc_out, y=None, eps=eps, order=order, clip_min=None, clip_max=None)
     
+    # Output dictionary to useful tf ops in the graph
     graph = dict(
         input_data = input_data,
         input_labels = input_labels,
@@ -79,14 +84,14 @@ def graph_builder_wrapper(num_classes, save_dir,
         fc_out = fc_out,
         delta = delta,
         delta_grad = delta_grad,
+        x_grad = x_grad,
         opt_step = opt_step,
         learning_rate = learning_rate,
         merged = merged,
         train_writer = train_writer,
         graph_writer = graph_writer,
         valid_writer = valid_writer,
-        saver = saver,
-        adv_x = adv_x
+        saver = saver
     )
     
     return graph
@@ -94,9 +99,9 @@ def graph_builder_wrapper(num_classes, save_dir,
 
 def train(Xtr, Ytr, graph, save_dir,
           val_set=None,
-          adv_robustness=False,
+          adv_robustness=None,
           Ip=15,
-          lambda_adv=0.5,
+          lambda_adv=2.0,
           lr_initial=0.01,
           seed=0,
           num_epochs=100,
@@ -162,13 +167,22 @@ def train(Xtr, Ytr, graph, save_dir,
                 x, y = Xtr_[i:i+batch_size], Ytr_[i:i+batch_size]
                 d = np.zeros(np.shape(x))
 
-                if adv_robustness:
-                    for _ in range(Ip):
-                        d_ =  sess.run(graph['delta_grad'], feed_dict={graph['input_data']: x,
-                                                                       graph['input_labels']: y,
-                                                                       graph['delta']: d})
-                        d -= lambda_adv*d_
-                
+                if adv_robustness == 'wrm':
+                    # Optimization using gradient descent as descriped in paper
+                    for ip in range(Ip):
+                        d -= (1/np.sqrt(1+ip))* sess.run(graph['delta_grad'],
+                                                        feed_dict={graph['input_data']: x,
+                                                                   graph['input_labels']: y,
+                                                                   graph['delta']: d})
+
+                elif adv_robustness == 'fgm':
+                    x_ = ad.fgm(x, graph, sess, eps=0.1, order=2)
+                    d = x-x_
+
+                elif adv_robustness == 'pgm':
+                    x_ = ad.pgm(x, graph, sess, eps=0.3, k=15, a=0.01, order=2)
+                    d = x-x_
+
                 feed_dict = {graph['input_data']: x, graph['input_labels']: y, graph['delta']: d}
                 training_loss_, training_acc_, _ = \
                     sess.run([graph['total_loss'], graph['total_acc'], graph['opt_step']],
@@ -182,11 +196,11 @@ def train(Xtr, Ytr, graph, save_dir,
                           %(epoch+1, load_epoch+num_epochs+1, time.time()-start,steps,
                             len(Xtr_)/batch_size, time.time()-t, training_loss_, training_acc_),
                           end='')            
-            
+
             if epoch%write_every == 0: # writing to tensorboard
                 summary = sess.run(graph['merged'], feed_dict=feed_dict)
                 graph['train_writer'].add_summary(summary, epoch)
-                
+
                 if val_set is not None: # make sure to keep the val_set small
                     feed_dict = {graph['input_data']: val_set['X'],
                                  graph['input_labels']: val_set['Y'],
@@ -206,10 +220,10 @@ def train(Xtr, Ytr, graph, save_dir,
                           %(early_stop_acc, epoch+1, load_epoch+num_epochs+1), end='')
                 break
 
-        print('\nDONE: epoch%s'%(epoch))
+        print('\nDONE: epoch%s\n'%(epoch))
         if not os.path.exists(os.path.join(save_dir, 'checkpoints', 'epoch%s'%(epoch))):
             graph['saver'].save(sess, os.path.join(save_dir, 'checkpoints', 'epoch%s'%(epoch)))
-            
+
     if verbose: print('')
     return training_losses, training_accs
 