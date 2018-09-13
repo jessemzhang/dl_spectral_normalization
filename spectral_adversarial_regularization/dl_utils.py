8df722fcd18432e774d607b657a4cf385cf50228
Jesse <jessez@stanford.edu>
2018-09-13 03:05:13 +0000

    fixed bug where adv total loss was not reported

--- spectral_adversarial_regularization/dl_utils.py
+++ spectral_adversarial_regularization/dl_utils.py
@@ -46,13 +46,13 @@ def graph_builder_wrapper(arch,
 
     input_data = tf.placeholder(tf.float32, shape=[None, 28, 28, num_channels], name='in_data')
     input_labels = tf.placeholder(tf.int64, shape=[None], name='in_labels')
-    
+
     fc_out = arch(input_data, num_classes=num_classes, wd=wd,
                   beta=beta, update_collection=update_collection)
-    
+
     # Loss and optimizer (with adversarial training options)
     learning_rate = tf.Variable(0.01, name='learning_rate', trainable=False)
-    
+
     if adv in ['wrm', 'fgm', 'pgm']:
         if adv == 'wrm':
             adv_x = ad.wrm(input_data, fc_out, eps=eps, order=2, model=arch, k=15,
@@ -67,17 +67,16 @@ def graph_builder_wrapper(arch,
         fc_out_adv = arch(adv_x, num_classes=num_classes, wd=wd,
                           beta=beta, update_collection=update_collection, reuse=True)
         total_loss = loss(fc_out_adv, input_labels)
+        total_acc = acc(fc_out_adv, input_labels)
         
     else:
         total_loss = loss(fc_out, input_labels)
+        total_acc = acc(fc_out, input_labels)
 
     if num_channels == 1: # For MNIST dataset
         opt_step = tf.train.AdamOptimizer(0.001).minimize(total_loss)
     else:
         opt_step = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(total_loss)
-
-    # Compute accuracy
-    total_acc = acc(fc_out, input_labels)
     
     # Output dictionary to useful tf ops in the graph
     graph = dict(
@@ -677,3 +676,41 @@ def get_overall_sn(load_dir, arch, num_classes=10, verbose=True, return_snorms=F
         return s_norms
 
     return(np.prod(s_norms.values()))
+
+
+from IPython.display import clear_output, Image, display, HTML
+
+def strip_consts(graph_def, max_const_size=32):
+    """Strip large constant values from graph_def."""
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add() 
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = "<stripped %d bytes>"%size
+    return strip_def
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+
+    iframe = """
+        <iframe seamless style="width:1200px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
